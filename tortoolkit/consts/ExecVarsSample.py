0145978c0a3e359a9b3d4e21115175f080a2a6e2
creuyhj <76662792+creuyhj@users.noreply.github.com>
2021-01-28 22:35:28 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,12 +5,12 @@ except:
         # TODO optimize for vps use fully - currently only heroku is focused
         # Set true if its VPS [currently not fully working]
         IS_VPS = False
-        API_HASH = "c3e3bdcaf3486d3222dcff67a6ea9a77"
-        API_ID = "1600667"
-        BOT_TOKEN = "1540012230:AAGPUnIgp4Ppwd6lqjb2zJ2R8-TUn2VRjjc"
-        BASE_URL_OF_BOT = "https://gautamleecherbot.herokuapp.com/"
+        API_HASH = "88d4da987b0288b8b0ea11729033bae0"
+        API_ID = 2229535
+        BOT_TOKEN = "1606470364:AAEd1jCy6VOFemzC5t-86XDOVQ1JgQp7uyQ"
+        BASE_URL_OF_BOT = "https://blacktortestbot.herokuapp.com/"
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [1030989359,-1001206349958]
+        ALD_USR = [1458912827,1467018377,1546675358,1444150889,1482853408,1333689035,992574970,-100374414317,-1001479070385,-100320640070]
         
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 40
@@ -28,10 +28,10 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "postgres://abdzbgwltfziwy:016aea57b883362ef1305b5d340f2ea83dcdf4e958e71f5cedbb020a72b5d81f@ec2-54-157-234-29.compute-1.amazonaws.com:5432/d8j8aptt91frmi"
+        DB_URI = "postgres://nrqzgvhqyigqkg:e2c0beefd33006c4c1e69dd63b9960149519f355e918f9836dee3c894726d590@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d6bl5irr8fmdf9"
         
         # The base direcory to which the files will be upload if using RCLONE
-        RCLONE_BASE_DIR = "/Bot Uploads/Leech Bot"
+        RCLONE_BASE_DIR = "/"
 
         # This value will be considered only if Rclone is True - this may be defied now ;)
         # Cuz at least one needs to be Ture at a time either RCLONE or Leech.
@@ -39,16 +39,16 @@ except:
 
         # Will be enabled once its set
         # For vps change it to True if config loaded
-        RCLONE_ENABLED = True
+        RCLONE_ENABLED = false
 
         # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
         DEFAULT_TIMEOUT = "leech"
 
         # For vps set path here or you can use runtime too
-        RCLONE_CONFIG = True
+        RCLONE_CONFIG = false
         
         # Name of the RCLONE drive from the config
-        DEF_RCLONE_DRIVE = "leech"
+        DEF_RCLONE_DRIVE = ""
 
         # Max size of the videos in playlist allowed
         MAX_YTPLAYLIST_SIZE = 200
