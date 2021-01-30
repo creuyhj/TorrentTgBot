660453871d9f32dab8e014748ef68c5bc59351f1
creuyhj <76662792+creuyhj@users.noreply.github.com>
2021-01-30 14:19:23 +0530

    Update getVars.py

--- tortoolkit/core/getVars.py
+++ tortoolkit/core/getVars.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) YashDK [yash-dk@github]
 
-from ..consts.ExecVarsSample import ExecVars
-#from ..core.database_handle import TorToolkitDB
-from tortoolkit import SessionVars
-import os
-
-def get_val(variable):
-    return SessionVars.get_var(variable)
+from ..consts.ExecVmple import ExecVars
+#from 
+sionVars.get_var(variable)
 
